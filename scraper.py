e77abba50f953677b80026ccbdc767b73fe5cc96
Parsa <parsamesg@gmail.com>
2021-03-11 19:09:13 +0000

    function checks if item is in stock

--- scraper.py
+++ scraper.py
@@ -8,10 +8,16 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36'
 }
 
-page = requests.get(URL, headers=headers)
+def check_availability():
+    page = requests.get(URL, headers=headers)
 
-soup = BeautifulSoup(page.content, 'html.parser')
+    soup = BeautifulSoup(page.content, 'html.parser')
 
-title = soup.find(id="productTitle").get_text()
+    title = soup.find(id="productTitle").get_text()
+    availability = soup.find(id="availability").get_text()
 
-print(title.strip())
+    if 'unavailable' not in availability:
+        send_mail()
+
+    print(availability.strip())
+    print(title.strip())
