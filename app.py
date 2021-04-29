3ccb9d44bbcd1a7e62a1f4f91d60efc2b10eea32
Parsa <parsamesg@gmail.com>
2021-04-29 19:02:38 +0100

    using environmental variables instead of modules

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
+import os
+import smtplib
+import time
+
 import requests
 from bs4 import BeautifulSoup
-import smtplib, time
-from login import from_email, to_email, password
 
 
 def check_availability(url, headers):
@@ -25,24 +27,25 @@ def send_mail():
     server.starttls()
     server.ehlo()
 
-    server.login(from_email, password)
+    server.login(os.environ.get('FROM_EMAIL_USER'),
+                 os.environ.get('FROM_EMAIL_PASS'))
 
     subject = "Item is back in stock!"
     body = "Check the Amazon link:\n\nhttps://www.amazon.co.uk/PlayStation-9395003-5-Console/dp/B08H95Y452/ref=sr_1_1?dchild=1&keywords=ps5&qid=1615488734&sr=8-1"
 
-
     msg = f"Subject: {subject}\n\n{body}"
 
     server.sendmail(
-        from_email,
-        to_email,
+        os.environ.get('FROM_EMAIL_USER'),
+        os.environ.get('TO_EMAIL_USER'),
         msg
     )
     print('EMAIL HAS BEEN SENT!')
-    
+
     server.quit()
 
+
 while True:
-    check_availability('https://www.amazon.co.uk/PlayStation-9395003-5-Console/dp/B08H95Y452/ref=sr_1_1?dchild=1&keywords=ps5&qid=1615488436&sr=8-1', 
-                        {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36'})
+    check_availability('https://www.amazon.co.uk/PlayStation-9395003-5-Console/dp/B08H95Y452/ref=sr_1_1?dchild=1&keywords=ps5&qid=1615488436&sr=8-1',
+                       {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36'})
     time.sleep(60)
